8e0c78587fcc5ef00b4495269ba7504f36ee7b5a
Laveena <laveenas372@gmail.com>
2026-08-03 23:01:29 +0530

    Time: 992 ms (77.44%), Space: 15.9 MB (81.2%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def stoneGameIII(self, stoneValue):
+        """
+        :type stoneValue: List[int]
+        :rtype: str
+        """
+        n = len(stoneValue)
+        dp = [0] * (n + 1)  # dp[i]: best score difference (current - opponent) from i..n-1
+        
+        for i in range(n - 1, -1, -1):
+            best = float('-inf')
+            take = 0
+            for k in range(3):
+                if i + k < n:
+                    take += stoneValue[i + k]
+                    best = max(best, take - dp[i + k + 1])
+            dp[i] = best
+        
+        if dp[0] > 0:
+            return "Alice"
+        elif dp[0] < 0:
+            return "Bob"
+        else:
+            return "Tie"
